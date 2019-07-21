233bfddc062d944c7f7af5ace63130f0026e43a9
Matthew C <m23.cullen@gmail.com>
2019-07-21 00:45:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,24 +25,10 @@ function viewCart() {
   var items = [];
   var prices = [];
   let i = 0;
-  while(cart.length > i);
+  while(cart.length > i){
   items.push(cart[i].itemName);
   prices.push(' at $' + cart[i].itemPrice);
-  for (var i = 0; i < cart.length; i++){
-     itemAndPrice = cart[i];
-     item = cart[i]["itemName"];
-     price = cart[i]["itemPrice"];
-     newCart.push(`${item} at $${price}`);
-  }
-  if(cart.length === 1){
-    return `${string}${newCart[0]}.`;
-  }
-  if(cart.length === 2){
-    return `${string}${newCart[0]}, and ${newCart[1]}.`;
-  }
-  if(cart.length >= 3){
-    
-  }
+  
 }
 
 function total() {
